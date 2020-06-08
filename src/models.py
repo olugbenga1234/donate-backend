a840e34ee888cbc34d881bc1f6db16dbe9d78bda
Olugbenga19 <34916630+hulkyboi@users.noreply.github.com>
2020-06-08 17:44:54 +0200

    commit

--- src/models.py
+++ src/models.py
@@ -16,12 +16,9 @@ class User(UserMixin, db.Model):
     address = db.Column(db.String(75), unique=False, nullable=False)
     state = db.Column(db.String(75), unique=False, nullable=False)
     lga = db.Column(db.String(75), unique=False, nullable=False)
-    phone = db.Column(db.Integer, nullable=True)
+    phone = db.Column(db.Integer, unique=True)
     bvn = db.Column(db.Numeric, nullable=False)
     usertype = db.Column(db.String, nullable=False, unique=False)
-    #farmer = db.Column(db.Boolean)
-    #seeder = db.Column(db.Boolean)
-    #admin = db.Column(db.Boolean)
     donated_amount = db.relationship('Donated', foreign_keys='Donated.donated_by_id',
                                      backref='donator', lazy=True)
 
